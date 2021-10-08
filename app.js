cc1f4969abaeef780363bed6f8de03ae9f8c4ee4
Kaia Scheirman <kfscheirman18@gmail.com>
2021-10-08 12:21:11 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const bill = document.getElementById("amount");
+const tipFive = document.getElementById("five");
+const tipTen = document.getElementById("ten");
+const tipFifteen = document.getElementById("fifteen");
+const tipTwentyfive = document.getElementById("twentyfive");
+const tipFifty = document.getElementById("fifty");
