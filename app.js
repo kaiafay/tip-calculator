fa33e62c1b442bacd7dcf05a88f82ca26633b99f
Kaia Scheirman <kfscheirman18@gmail.com>
2021-10-15 15:41:36 -0700

    add styling on active button

--- app.js
+++ app.js
@@ -4,3 +4,22 @@ const tipTen = document.getElementById("ten");
 const tipFifteen = document.getElementById("fifteen");
 const tipTwentyfive = document.getElementById("twentyfive");
 const tipFifty = document.getElementById("fifty");
+const tipAmount = document.getElementById("tipamount");
+const total = document.getElementById("total");
+const reset = document.getElementById("reset");
+
+const calcTip = () => {
+  total.textContent = '$' + bill
+}
+
+const getBillAmount = () => {
+    bill = document.getElementById("amount");
+    if (!bill.value || Number(bill.value) === 0) {
+        billValue = 0;
+        calcTip();
+        return;
+    }
+    billValue = Number(bill.value);
+    calcTip();
+}
+
