2730fd3eda36892607aca6a1de909b6328756f3c
Kaia Scheirman <kfscheirman18@gmail.com>
2022-02-01 12:23:51 -0800

    fix buttons

--- app.js
+++ app.js
@@ -1,46 +1,25 @@
 const bill = document.getElementById("amount"); // bill amount
-const tipAmount = document.getElementsById("tipamount"); // total tip amount
-const total = document.getElementsById("total"); // total bill amount 
+const tipAmountEl = document.getElementById("tipamount"); // total tip amount
+const total = document.getElementById("total"); // total bill amount 
 const reset = document.getElementById("reset"); // reset button
 const buttons = document.querySelectorAll(".tipButton"); // tip buttons
 
-// function that presents total bill amount
-// const calcTip = () => {
-//   total.textContent = '$' + bill
-// }
-
-// // function that calculates tip
-// const getBillAmount = () => {
-//     bill = document.getElementById("amount");
-//     if (!bill.value || Number(bill.value) === 0) {
-//         billValue = 0;
-//         calcTip();
-//         return;
-//     }
-//     billValue = Number(bill.value);
-//     calcTip();
-// }
-
-// tipFive.onclick = function() {
-//   alert("I AM CLICKED!");
-// }
-
 function calcTip(bill, tipAmount) {
-  let tipAmount = bill * (tipAmount / 100);
+  tipAmount = bill * (tipAmount / 100);
   let tip = Math.floor(tipAmount * 100) / 100;
   tip = tip.toFixed(2);
 
 let totalAmount = tipAmount + bill; 
 totalAmount = totalAmount.toFixed(2);
 
-tipAmount.innerHTML = `$${tip}`;
+tipAmountEl.innerHTML = `$${tip}`;
 total.innerHTML = `$${totalAmount}`;
 }
 
-console.log('Here');
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    console.log("CLICK");
+    button.removeAttribute('class', 'active');
+    e.target.setAttribute('class', 'tipButton active');
     let tipValue = e.target.innerText;
     tipValue = tipValue.substr(0, tipValue.length - 1);
 
